a51089f34fcec89b52bdbf3965259ec52cdb2e66
Hrushiprada Swain <hrushipradaswain@gmail.com>
2026-08-26 13:47:09 +0530

    First Springboot Console based project

---
+++ Application.java
@@ -0,0 +1,22 @@
+package in.hrushiit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	
+	@GetMapping("/")
+	public String msg() {
+		return "welcome to Hrushi IT";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Application Java class is Loaded");
+	}
+
+}
